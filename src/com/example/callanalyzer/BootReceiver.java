1174bd2a8f508a7b4d55687613ef55706ac99415
Kishor Mohite <kishormohite888@gmail.com>
2013-07-30 19:35:29 +0530

    modified BootReceiver to start background service on boot

--- src/com/example/callanalyzer/BootReceiver.java
+++ src/com/example/callanalyzer/BootReceiver.java
@@ -1,17 +1,20 @@
 package com.example.callanalyzer;
 
+import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
-public class BootReceiver extends BroadcastReceiver {
+public class BootReceiver extends BroadcastReceiver{
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
-		
-		Log.w("SERVICE", "starting service...");
-		context.startService(new Intent(context, BootServiceClass.class));
+	public void onReceive(Context arg0, Intent arg1) {
+		Log.i("SERVICE","STARTED");
+		Toast.makeText(arg0, "Call Analyzer is working in background", 2000).show();
+		Intent service = new Intent(arg0, BackgroundService.class);
+		arg0.startService(service);
 	}
 
 }
