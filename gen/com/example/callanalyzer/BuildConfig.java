111cd72e2367e85cf5aa5ede799bbafc7684d681
Kishor Mohite <kishormohite888@gmail.com>
2013-07-30 19:17:18 +0530

    fixed untracked files

--- gen/com/example/callanalyzer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.callanalyzer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/example/callanalyzer/BootReceiver.java
+++ src/com/example/callanalyzer/BootReceiver.java
@@ -3,17 +3,15 @@ package com.example.callanalyzer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
-public class BootReceiver {
+public class BootReceiver extends BroadcastReceiver {
 
-	public class MyReceiver extends BroadcastReceiver {
-		;
-		@Override
-		public void onReceive(Context context, Intent intent) {
-
-			Intent myIntent = new Intent(context, BootServiceClass.class);
-			context.startService(myIntent);
-		}
-
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		Log.w("SERVICE", "starting service...");
+		context.startService(new Intent(context, BootServiceClass.class));
 	}
+
 }
